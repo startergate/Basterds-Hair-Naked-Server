2c0556f21185d88e55d7bea5397719ba7fe31ed2
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-18 02:24:43 +0900

    [INIT] Re-INIT

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basterds_hair_naked.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basterds_stat.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
