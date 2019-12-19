b027176fcfa897118b4081d9c3bd8ba6e312a045
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-19 11:30:38 +0900

    [UPDATE] 더미 데이터

--- v1/views.py
+++ v1/views.py
@@ -9,7 +9,57 @@ def index(request):
 
 
 def get_profile(request):
-    return JsonResponse({})
+    return JsonResponse({
+        "pid": "1234567890123456",
+        "total": {
+            "score": 10000000,
+            "playtime": 42342423,
+            "turn_count": 8000,
+            "match_count": 1000,
+            "win_count": 500,
+            "spawned_alias": 10000,
+            "killed_alias": 12000,
+            "killed_hostiles": 10000,
+            "damage": 3000000,
+            "heal": 250000
+        },
+        "1": {
+            "score": 10000000,
+            "playtime": 42342423,
+            "turn_count": 8000,
+            "match_count": 1000,
+            "win_count": 500,
+            "spawned_alias": 10000,
+            "killed_alias": 12000,
+            "killed_hostiles": 10000,
+            "damage": 3000000,
+            "heal": 250000
+        },
+        "2": {
+            "score": 10000000,
+            "playtime": 42342423,
+            "turn_count": 8000,
+            "match_count": 1000,
+            "win_count": 500,
+            "spawned_alias": 10000,
+            "killed_alias": 12000,
+            "killed_hostiles": 10000,
+            "damage": 3000000,
+            "heal": 250000
+        },
+        "3": {
+            "score": 10000000,
+            "playtime": 42342423,
+            "turn_count": 8000,
+            "match_count": 1000,
+            "win_count": 500,
+            "spawned_alias": 10000,
+            "killed_alias": 12000,
+            "killed_hostiles": 10000,
+            "damage": 3000000,
+            "heal": 250000
+        }
+    })
 
 
 def get_match(request):
