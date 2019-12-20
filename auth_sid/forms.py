0b5a12daccedf282070968e5f5975aea956a7b0e
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-20 09:21:07 +0900

    [UPDATE] User Model

--- auth_sid/forms.py
+++ auth_sid/forms.py
@@ -6,5 +6,5 @@ from auth_sid.models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('pid', 'uid')
+        fields = ('pid', 'id')
         app_label = 'auth_sid'
