5c398b2fea21ba91b01b966f7837b8bce5dc6f12
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-26 14:40:38 +0900

    [UPDATE] User Model

--- auth_sid/models.py
+++ auth_sid/models.py
@@ -6,5 +6,5 @@ from django.db import models
 class User(models.Model):
     def __str__(self):
         return self.id
-    pid = models.CharField(max_length=64, primary_key=True)
+    pid = models.CharField(max_length=32, primary_key=True)
     id = models.CharField(max_length=20, null=False, unique=True)
