e7e2e63741e7b768614e51460dbb9abc0556f2c1
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-27 12:56:46 +0900

    [UPDATE] Match Model

--- api/forms.py
+++ api/forms.py
@@ -6,8 +6,8 @@ from api.models import Match, Object, Action
 class MatchForm(forms.ModelForm):
     class Meta:
         model = Match
-        fields = ('matchid', 'pid1', 'pid2', 'status', 'played_as', 'score', 'playtime', 'turn_count',
-                  'spawned_alias', 'killed_alias', 'killed_hostiles', 'damage', 'heal', 'created_at', 'terminated_at')
+        fields = ('matchid', 'player1', 'played_as2', 'status', 'played_as1', 'played_as2', 'score1', 'score2', 'playtime', 'turn_count',
+                  'spawned1', 'killed1', 'spawned2', 'killed2', 'damage1', 'heal1', 'damage2', 'heal2', 'created_at', 'terminated_at')
         app_label = 'api'
 
 
